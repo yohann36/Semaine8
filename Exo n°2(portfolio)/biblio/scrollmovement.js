560588a1f0e6341a60b2c2d6d080328cf661d0f5
yohann36 <yohann36a@gmail.com>
2015-12-13 12:14:01 +0000

    disons fini pour avant sur d'autre choses

---
+++ Exo n°2(portfolio)/biblio/scrollmovement.js
@@ -0,0 +1,12 @@
+$(function () {
+ $('nav a').on('click', function (e) {
+                e.preventDefault();
+                // hash permet de cibler le href de nos liens.
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 1000, function () {
+                    window.location.hash = hash;
+                });
+            });
+        });
