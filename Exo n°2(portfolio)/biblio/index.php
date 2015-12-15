122ead1e6f5040e8fd5d634eabe41e7144c31b79
yohann36 <yohann36a@gmail.com>
2015-12-15 01:04:14 +0000

    essai php post

--- Exo n°2(portfolio)/biblio/index.php
+++ Exo n°2(portfolio)/biblio/index.php
@@ -1,3 +1,44 @@
-<?php
-            echo("Nom : " . $_GET['Nom'] . " Prénom : " . $_GET['prenom'] . " Email : " . $_GET['email']. " Telephone : " . $_GET['telephone']);
-        ?>
+<!DOCTYPE html>                  
+<html>
+    <head>        
+                        
+        <meta charset="utf-8"/>   
+        
+        <!--S'assure que EI est à jour-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <!--Rendu sans zoom sur les appareil mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="description" content="CV">
+        <meta name="author" content="Yohann ALAPETITE">
+        
+        <!--link vers bootstrap-->
+        <link href="biblio/bootstrap.css" rel="stylesheet" >
+        
+        
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+      
+      
+        <title>CV Interactif</title>
+       
+    </head>
+  <body>
+    <p>
+      <?php
+
+  echo 'Bonjour, ' . $_POST ['prenom'] . ' ' . $_POST['nom'] . ', votre commentaire : ' . $_POST['comment'] . ' a bien été enregister.';
+ 
+?>
+</p>
+  </body>  
+  
+  
+  </html>
+
+
